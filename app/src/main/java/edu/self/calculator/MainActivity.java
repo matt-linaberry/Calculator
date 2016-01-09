c9a23a060da7d77c375425376babced699ffecdf
Matt Linaberry <mtlinaberry@gmail.com>
2016-01-08 22:20:42 -0500

    Calculator Complete!
    This completes the calculator section in the udemy course.

--- app/src/main/java/edu/self/calculator/MainActivity.java
+++ app/src/main/java/edu/self/calculator/MainActivity.java
@@ -1,15 +1,27 @@
 package edu.self.calculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnDivision;
+    private Button btnMultiplication;
+    private Button btnClear;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,97 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        operand1 = (EditText)findViewById(R.id.editOperand1);
+        operand2 = (EditText)findViewById(R.id.editOperand2);
+
+        btnAddition = (Button)findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        btnDivision = (Button)findViewById(R.id.btnDivision);
+        btnClear = (Button)findViewById(R.id.btnClear);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    // grab the two operands, then add them
+                    double op1 = Double.parseDouble(operand1.getText().toString());
+                    double op2 = Double.parseDouble(operand2.getText().toString());
+
+                    double total = op1 + op2;
+                    txtResult.setText(Double.toString(total));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+
+                    // grab the two operands, then subtract them
+                    double op1 = Double.parseDouble(operand1.getText().toString());
+                    double op2 = Double.parseDouble(operand2.getText().toString());
+
+                    double total = op1 - op2;
+                    txtResult.setText(Double.toString(total));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+
+                    // grab the two operands, then multiply them
+                    double op1 = Double.parseDouble(operand1.getText().toString());
+                    double op2 = Double.parseDouble(operand2.getText().toString());
+
+                    double total = op1 * op2;
+                    txtResult.setText(Double.toString(total));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+
+                    // grab the two operands, then divide them
+                    double op1 = Double.parseDouble(operand1.getText().toString());
+                    double op2 = Double.parseDouble(operand2.getText().toString());
+
+                    double total = op1 / op2;
+                    txtResult.setText(Double.toString(total));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.setText("");
+                operand2.setText("");
+                txtResult.setText("0.0");
+                operand1.requestFocus();
+            }
+        });
 
     }
 
